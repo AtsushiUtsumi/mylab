9d26b0247df2be4aff59f6e946de2979eb1156fa
AtsushiUtsumi <debtgx103@gmail.com>
2024-10-13 14:10:09 +0900

    使わないimport削除

--- src/main/java/com/example/AppConfig.java
+++ src/main/java/com/example/AppConfig.java
@@ -1,15 +1,8 @@
 package com.example;
 
-import net.sf.log4jdbc.sql.jdbcapi.DataSourceSpy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;// TOOD : これあとで調べる
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class AppConfig {
